9feb5a43e11509651b4508e749d81ed39dae570b
Krpvleeaan <84187515+Krpvleeaan@users.noreply.github.com>
2022-03-21 21:49:22 +0300

    Add files via upload
    Algorithm for obtaining prime numbers

---
+++ Eratosfen/Ertosfen/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Ertosfen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(134, 101);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(267, 39);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(458, 101);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(278, 264);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox3.Location = new System.Drawing.Point(134, 30);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(225, 35);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "   Введите верхнюю границу";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(458, 29);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(191, 52);
+            this.richTextBox4.TabIndex = 4;
+            this.richTextBox4.Text = "Все простые числа от 0 до верхней границы ниже";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ Eratosfen/Ertosfen/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ertosfen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {}
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input;
+            richTextBox2.Text = "";
+            if (Int32.TryParse(richTextBox1.Text, out input) == false)
+            {
+                MessageBox.Show("Некорректное число");
+                return;
+            }
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Введите число");
+                return;
+            }
+            if (input < 2)
+                return;
+            if (input == 2) 
+            {
+                richTextBox2.Text += "2";
+                return;
+            }
+            if (input <= 4)
+            {
+                richTextBox2.Text += "2 3";
+                return;
+            }
+            if (input <= 5)
+            {
+                richTextBox2.Text += "2 3 5";
+                return;
+            }
+            if (input > 2147483590)
+            {
+                MessageBox.Show("Вы ввели слишком большое число");
+                return;
+            }
+            DateTime starttime = DateTime.Now;
+            int n = Convert.ToInt32(richTextBox1.Text);
+            richTextBox2.Text = "";
+            bool[] A = new bool[n + 1];
+            for (int i = 2; i < n + 1; i++)
+                A[i] = true;
+            for (int i = 2; i < Math.Sqrt(n + 1) + 1; ++i)
+            {
+                if (A[i])
+                {
+                    for (int j = i * i; j < n + 1; j += i)
+                    {
+                        A[j] = false;
+                    }
+                }
+            }
+            StreamWriter wr = new StreamWriter("temp.txt");
+            for (int i = 2; i < n + 1; i++)
+            {
+                if (A[i])
+                {
+                    wr.Write(i.ToString() + ' ');
+                }
+            }
+            wr.Close();
+            StreamReader rd = new StreamReader("temp.txt");
+            richTextBox2.Text = rd.ReadToEnd();
+            rd.Close();
+            DateTime endtime = DateTime.Now;
+            MessageBox.Show("Выполнено");
+            MessageBox.Show((endtime - starttime).ToString());
+        }
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
